ff9e6a584d39665cfc868dfee28382622810c3dc
drakari7 <shreyash.ravi@gmail.com>
2022-07-22 17:52:00 +0200

    add aliases

---
+++ .aliases.zsh
@@ -0,0 +1,11 @@
+alias zrc="nvim ~/.zshrc"
+alias out="./a.out"
+alias spa="spicetify apply"
+alias spcolor="spicetify config color_scheme"
+alias sptheme="spicetify config current_theme"
+alias hgrep="history | rg"
+alias zhist="nvim ~/.zsh_history"
+alias kitconf="nvim ~/.config/kitty/kitty.conf"
+alias kit_ssh="kitty +kitten ssh"
+
+unalias -m 'l*'
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ My dotfiles for common unix and mac applications. Dotfiles included
 are listed here,
 
     1. .zshrc
+    2. .aliases.zsh - regularly used aliases
